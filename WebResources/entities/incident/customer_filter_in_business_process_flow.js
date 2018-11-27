f43c81ea7a7f3fc500c5bc37815ec446c751947a
Tyler Hogsett <tyhogset@microsoft.com>
2018-11-26 20:57:04 -0800

    added solution files

---
+++ WebResources/entities/incident/customer_filter_in_business_process_flow.js
@@ -0,0 +1,15 @@
+﻿function enforceCustomerFilterInBusinessProcessFlow() {
+    var customerControl = Xrm.Page.getControl("header_process_customer");
+    var contactControl = Xrm.Page.getControl("header_process_contact");
+    if (customerControl !== null && contactControl !== null) {
+        contactControl.addPreSearch(function () {
+            var customerValue = customerControl.getAttribute().getValue();
+            contactControl.addCustomFilter(
+                "<filter>" +
+                    (customerValue === null ?"" :
+                        "<condition attribute=\"parentcustomerid\" operator=\"eq\" value=\"" + customerValue[0].id + "\" />") +
+                "</filter>"
+            );
+        });
+    }
+}
